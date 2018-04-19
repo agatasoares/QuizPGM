67f15d158baf4ab24767c3e035f1d363c637c45e
eloisanc <36730947+eloisanc@users.noreply.github.com>
2018-04-19 10:42:59 -0300

    Revert "Revert "Categoria PA 2""
    This reverts commit c1cb8af5d16ef2cc3ab58935e47a3d5f074c02f2.

---
+++ QuestiPGM.Desk/categoriapa2.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace QuestiPGM.Desk
+{
+    partial class categoriapa2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(613, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quais desses países fizeram parte da Tríplice Entente durante a Primeira Guerra M" +
+    "undial?";
+            // 
+            // categoriapa2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(784, 462);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "categoriapa2";
+            this.Text = "categoriapa2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
