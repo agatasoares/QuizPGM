f3bbe47e908e9f7eea5829e217d1dbd4f8503156
Agatha Soares <36730994+agatasoares@users.noreply.github.com>
2018-04-26 08:54:01 -0300

    Tela tecnologia

---
+++ QuestiPGM.Desk/ciencia_tec.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuestiPGM.Desk
+{
+    public partial class ciencia_tec : Form
+    {
+        public ciencia_tec()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (alternativa_1.Checked)
+            {
+                MessageBox.Show("Alternativa errada!", MessageBoxButtons.OK;)
+            }
+        }  
+    }
+}
